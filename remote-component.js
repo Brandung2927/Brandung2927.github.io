e27b0563e1959ba25e056010c2ea4c375b448b2c
Matcha2927 <1539904941@qq.com>
2024-07-04 17:34:08 +0800

    deploy

---
+++ remote-component.js
@@ -0,0 +1,13 @@
+export default {
+  template: `
+      <div>
+        <h3>This is a remote component</h3>
+        <p>{{ message }}</p>
+      </div>
+    `,
+  setup() {
+    return {
+      message: 'Hello from the remote component!'
+    }
+  }
+}
