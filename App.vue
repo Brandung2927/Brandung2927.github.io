385d08f7e19ba263ff4a51addfab110c1fe71165
Matcha2927 <1539904941@qq.com>
2024-07-04 17:30:52 +0800

    deploy

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>This is a remote component</h3>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello from the remote component!');
+</script>
+
+<style scoped>
+h3 {
+  color: #42b983;
+}
+</style>
